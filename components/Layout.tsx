309f78fee9e62d3eaf9598e4b3394cd02539c501
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-04-05 21:44:54 +0200

    start

---
+++ components/Layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar, Footer } from "@/components";
+
+const Layout = ({ children }: any) => {
+  return (
+    <div className="max-w-[1850px] w-full mx-auto  ">
+      <Navbar />
+      <div className="max-w-[1650px] w-full mx-auto ">
+        <div>{children}</div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { aboutUs, NavbarItems } from "@/Datas";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full  border-b-2 h-[100px] flex flex-col justify-center ">
+      <div className="max-w-[1650px] w-full flex justify-between  mx-auto  items-center">
+        <h2 className="cursor-pointer text-[30px] text-[red] font-bold md:text-[35px]">
+          Digiti
+        </h2>
+        <div className="flex gap-5">
+          <p className="cursor-pointer">shopIcon</p>
+          <p className="cursor-pointer">Login</p>
+        </div>
+      </div>
+      <div className="flex justify-around">
+        <div className="flex   gap-3 text-gray-500 ">
+          {NavbarItems.map((item, index) => (
+            <p className="cursor-pointer" key={index}>
+              {item}
+            </p>
+          ))}
+        </div>
+        <div className="flex gap-3  text-gray-500">
+          {aboutUs.map((item, index) => (
+            <p className="cursor-pointer" key={index}>
+              {item}
+            </p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
