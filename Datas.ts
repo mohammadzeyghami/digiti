650297920915a6144c8d2b916f1f273d3cc728a5
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-04-06 20:12:28 +0200

    fix  swiper bug

--- Datas.ts
+++ Datas.ts
@@ -37,8 +37,8 @@ export const Categories: { image: any; title: string }[] = [
 
 export const offerProducts = [
   { image: iphone_1, title: "ایفون 11", price: "600$", offer: "570$" },
-  { image: sam1, title: "سامسونگ اس 21", price: "800$", offer: "780$" },
-  { image: ps1, title: "پلی استیشن 4", price: "400$", offer: "375$" },
+  { image: sam1, title: "s21", price: "800$", offer: "780$" },
+  { image: ps1, title: "پی اس 4", price: "400$", offer: "375$" },
   { image: iphone_2, title: "ایفون 12 ", price: "700$", offer: "685$" },
   { image: iphone_3, title: "ایفون 13", price: "900$", offer: "875$" },
   { image: xbox1, title: "ایکس باکس 1 ", price: "400$", offer: "380$" },
--- components/ProductOffer.tsx
+++ components/ProductOffer.tsx
@@ -3,17 +3,19 @@ import Image from "next/image";
 const ProductOffer = ({ product }: any) => {
   const { image, title, text, price, offer } = product;
   return (
-    <div className="xl:h-[300px] xl:w-[230px] mx-[5px]  ml-0 bg-gray-400 rounded-md flex flex-col p-[8px]">
+    <div className="xl:h-[300px] xl:w-[230px] mx-[5px]    ml-0 bg-gray-400 rounded-md flex flex-col p-[8px]">
       <Image
         className="rounded-md w-full h-[60%] "
         src={image}
         alt="productImage"
       />
       <div className="flex flex-col">
-        <h3 className="text-black font-semibold text-center mt-2">{title}</h3>
+        <h3 className="text-black font-semibold text-center mt-2 text-[14px]">
+          {title}
+        </h3>
         <p className="text-gray-500 text-center mt-4">{text}</p>
         <div className="text-center ">
-          <p className="text-gray-600 font-bold line-through  ">{price}</p>
+          <p className="text-gray-600 font-bold line-through ">{price}</p>
         </div>
         <p className="text-white bg-red-600 w-[80px] flex flex-end rounded-full font-bold justify-center ">
           {offer}
