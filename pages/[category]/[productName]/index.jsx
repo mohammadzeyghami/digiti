06a6ce27cf645ed89d4ac072d34702997f79d470
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-04-09 20:51:52 +0200

    Update index.jsx

--- pages/[category]/[productName]/index.jsx
+++ pages/[category]/[productName]/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import { AiFillStar } from "react-icons/ai";
 
 const index = () => {
   return (
@@ -17,7 +18,22 @@ const index = () => {
           <h3 className="font-semibold text-[22px]">گوشی ایفون فلان</h3>
           <div className="w-full flex items-center  ">
             <p className="text-gray-300  w-[170px]"> Lorem ipsum dolor</p>
+
             <div className=" bg-gray-300 h-[1px] w-full "></div>
+          </div>
+          <div className="flex items-center">
+            <AiFillStar className="text-yellow-400" size={25} />
+            <p className="text-[16px] text-gray-400">3.8</p>
+          </div>
+          <div className="flex gap-[10px]">
+            <div className="text-white font-bold bg-gray-600 w-[18px] h-[18px] rounded-full flex justify-center items-center">
+              !
+            </div>
+            <p className="text-gray-400 text-[14px]">
+              درخواست مرجوع کردن کالا در گروه تخته با دلیل "انصراف از خرید" تنها
+              در صورتی قابل تایید است که کالا در شرایط اولیه باشد (در صورت پلمپ
+              بودن، کالا نباید باز شده باشد).
+            </p>
           </div>
         </div>
         <div></div>{" "}
